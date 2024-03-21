7fd0553ddd9cc193b4691deb62c6bea4131cba0a
Lucho Web <luchoweb.dev@gmail.com>
2024-03-21 10:53:08 -0500

    javascript code improvement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,36 +1,40 @@
-const resetSidebarClassName = () => {
-  const mainDashboard = document.querySelector(".dashboard");
-  mainDashboard.classList.remove(...["sidebar-open", "sidebar-close"]);
-};
-
-const closeOpenSidebar = () => {
-  const closeSidebarBtn = document.querySelector(".sidebar-arrow");
-
-  closeSidebarBtn.addEventListener("click", () => {
-    const isMobile = window.innerWidth <= 768;
+const Dashboard = {
+  resetSidebarClassName: () => {
     const mainDashboard = document.querySelector(".dashboard");
-    const sidebarClassName = isMobile ? "sidebar-open" : "sidebar-close";
+    mainDashboard.classList.remove(...["sidebar-open", "sidebar-close"]);
+  },
 
-    mainDashboard.classList.toggle(sidebarClassName);
-  });
-};
+  closeOpenSidebar: () => {
+    const closeSidebarBtn = document.querySelector(".sidebar-arrow");
 
-const selectFiles = () => {
-  const checkboxList = document.querySelectorAll('table td input[type="checkbox"]');
-  const inputCheckAll = document.getElementById("check-all");
+    closeSidebarBtn.addEventListener("click", () => {
+      const isMobile = window.innerWidth <= 768;
+      const mainDashboard = document.querySelector(".dashboard");
+      const sidebarClassName = isMobile ? "sidebar-open" : "sidebar-close";
 
-  inputCheckAll.addEventListener("click", () => {
-    for (const checkbox of checkboxList) {
-      checkbox.checked = inputCheckAll.checked;
-    }
-  });
+      mainDashboard.classList.toggle(sidebarClassName);
+    });
+  },
+
+  selectFiles: () => {
+    const checkboxList = document.querySelectorAll(
+      'table td input[type="checkbox"]'
+    );
+    const inputCheckAll = document.getElementById("check-all");
+
+    inputCheckAll.addEventListener("click", () => {
+      for (const checkbox of checkboxList) {
+        checkbox.checked = inputCheckAll.checked;
+      }
+    });
+  },
 };
 
 window.addEventListener("load", () => {
-  closeOpenSidebar();
-  selectFiles();
+  Dashboard.closeOpenSidebar();
+  Dashboard.selectFiles();
 });
 
 window.addEventListener("resize", () => {
-  resetSidebarClassName();
+  Dashboard.resetSidebarClassName();
 });
