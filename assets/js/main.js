ebdbd70d11111d88cda4f7b11f1d6ee746d60dea
Lucho Web <luchoweb.dev@gmail.com>
2024-03-21 10:43:13 -0500

    sidebar code improvement

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,36 @@
-const closeOpenSidebar = () => {
+const resetSidebarClassName = () => {
   const mainDashboard = document.querySelector(".dashboard");
+  mainDashboard.classList.remove(...["sidebar-open", "sidebar-close"]);
+};
+
+const closeOpenSidebar = () => {
   const closeSidebarBtn = document.querySelector(".sidebar-arrow");
 
-  const sidebarCloseClassName = "sidebar-close";
-  const sidebarOpenClassName = "sidebar-open";
-
   closeSidebarBtn.addEventListener("click", () => {
-    if (mainDashboard.classList.contains(sidebarCloseClassName)) {
-      mainDashboard.classList.remove(sidebarCloseClassName);
-      mainDashboard.classList.add(sidebarOpenClassName);
-      return;
-    }
+    const isMobile = window.innerWidth <= 768;
+    const mainDashboard = document.querySelector(".dashboard");
+    const sidebarClassName = isMobile ? "sidebar-open" : "sidebar-close";
 
-    mainDashboard.classList.add(sidebarCloseClassName);
-    mainDashboard.classList.remove(sidebarOpenClassName);
+    mainDashboard.classList.toggle(sidebarClassName);
   });
 };
 
 const selectFiles = () => {
-  const checkboxes = document.querySelectorAll('td input[type="checkbox"]');
-  const checkAll = document.getElementById("check-all");
+  const checkboxList = document.querySelectorAll('table td input[type="checkbox"]');
+  const inputCheckAll = document.getElementById("check-all");
 
-  checkAll.addEventListener("click", () => {
-    for (const checkbox of checkboxes) {
-      checkbox.checked = checkAll.checked;
+  inputCheckAll.addEventListener("click", () => {
+    for (const checkbox of checkboxList) {
+      checkbox.checked = inputCheckAll.checked;
     }
   });
-}
+};
 
 window.addEventListener("load", () => {
   closeOpenSidebar();
   selectFiles();
+});
+
+window.addEventListener("resize", () => {
+  resetSidebarClassName();
 });
